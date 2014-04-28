177e9c521339dd239f84428a743058ddb51efe04
admin <admin@localhost>
2014-04-28 06:31:53 +0000

    Solr changes for CRUD

--- cSolr.php
+++ cSolr.php
@@ -5,18 +5,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//Delete all http://127.0.0.1:8080/solr/collection1/update?commit=true%20-d%20%27%3Cdelete%3E%3Cquery%3E*:*%3C/query%3E%3C/delete%3E%27
-//include '../mquery/cModel.php';
-include AppQueryModule . 'cModel.php';
+//Delete all http://127.0.0.1:8080/solr/collection1/update?commit=true -d '<delete><query>*:*</query></delete>'
+include '../mquery/cModel.php';
 
-class cSolr implements cModel
-    {
+//include AppRoot . AppQueryModule . 'cModel.php';
 
-    public $columns;
+class cSolr implements cModel {
+
+    public $column;
     private $query;
     private $client;
     private $result;
     private $document;
+    public $condition;
 
     function __construct($core = "") {
         $connectionProps = array();
@@ -32,11 +33,14 @@ class cSolr implements cModel
 
     function read() {
 
-        if (is_array($this->columns)) {
-            foreach ($this->columns as $column) {
+        if (is_array($this->column)) {
+            foreach ($this->column as $column) {
                 $this->query->addField($column);
             }
         }
+        if ($this->condition == "")
+            $this->condition = '*:*';
+        $this->query->setQuery($this->condition);
         $this->result = $this->client
                 ->query($this->query);
         return $this->result->getResponse();
@@ -44,8 +48,8 @@ class cSolr implements cModel
 
     function create() {
 
-        if (is_array($this->columns)) {
-            foreach ($this->columns as $column => $value) {
+        if (is_array($this->column)) {
+            foreach ($this->column as $column => $value) {
                 $this->document->addField($column, $value);
             }
             $this->result = $this->client->addDocument($this->document);
@@ -60,8 +64,8 @@ class cSolr implements cModel
 
     function delete() {
 
-        $this->client->deleteByQueries($this->condition);
-        return $this->client->commit();
+        $this->client->deleteByQuery($this->condition);
+        return $this->commit();
     }
 
     public function addOrderBy($orderby) {
@@ -80,12 +84,23 @@ class cSolr implements cModel
 
     public function addWhereCondition($condition) {
 
+        $this->condition = $condition;
+//        foreach ($this->condition as $key => $value) {
+//
+//        }
         return $this;
     }
 
+    public function commit() {
+        $solrAddress = SOLR_HOST . ':' . SOLR_PORT . SOLR_PATH;
+        $output = array();
+        $response = exec('curl ' . $solrAddress . '/update?commit=true', $output);
+        return $output;
     }
 
-$cSolrObj = new cSolr("collection1");
+}
 
-print_r($cSolrObj->addLimit(50)->addOffset(0)->read());
+//$cSolrObj = new cSolr("collection1");
+//
+//print_r($cSolrObj->addLimit(50)->addOffset(0)->read());
 ?>
